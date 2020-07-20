841d6d7f6aaf1127e648b4aa66b7b642227a1d62
MugishaU <muwiragiye123@gmail.com>
2020-07-20 13:51:30 +0100

    make reverse via server work

--- client.js
+++ client.js
@@ -1,3 +1,19 @@
-fetch("http://localhost:3000/")
-  .then((r) => r.json())
-  .then(console.log);
+const form = document.getElementById("string");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let originalText = event.target.input.value;
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(originalText),
+  };
+
+  fetch("http://localhost:3000/", options)
+    .then((r) => r.json())
+    .then((data) => {
+      result.textContent = data;
+    })
+    .catch(console.warn);
+});
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const server = express();
 server.use(cors());
+server.use(bodyParser.text());
 const port = 3000;
 
 server.listen(port, () =>
   console.log(`Express server running from http://localhost:${port}`)
 );
+
+server.post("/", (req, res) => {
+  let originalText = JSON.parse(req.body);
+  let reversed = originalText.split("").reverse().join("");
+  res.send(JSON.stringify(reversed));
+});
