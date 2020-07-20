470867d1ca99da192694f0825fa50cb0748d1bc4
MugishaU <muwiragiye123@gmail.com>
2020-07-20 15:18:43 +0100

    add meme route and functionality

--- client.js
+++ client.js
@@ -1,7 +1,8 @@
-const form = document.getElementById("string");
+const r_form = document.getElementById("reverse_form");
 const result = document.getElementById("result");
+const m_form = document.getElementById("meme_form");
 
-form.addEventListener("submit", (event) => {
+r_form.addEventListener("submit", (event) => {
   event.preventDefault();
   let originalText = event.target.input.value;
 
@@ -10,7 +11,24 @@ form.addEventListener("submit", (event) => {
     body: JSON.stringify(originalText),
   };
 
-  fetch("http://localhost:3000/", options)
+  fetch("http://localhost:3000/reverse", options)
+    .then((r) => r.json())
+    .then((data) => {
+      result.textContent = data;
+    })
+    .catch(console.warn);
+});
+
+m_form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let originalText = event.target.input.value;
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(originalText),
+  };
+
+  fetch("http://localhost:3000/meme", options)
     .then((r) => r.json())
     .then((data) => {
       result.textContent = data;
--- server.js
+++ server.js
@@ -10,8 +10,28 @@ server.listen(port, () =>
   console.log(`Express server running from http://localhost:${port}`)
 );
 
-server.post("/", (req, res) => {
+server.post("/reverse", (req, res) => {
   let originalText = JSON.parse(req.body);
   let reversed = originalText.split("").reverse().join("");
   res.send(JSON.stringify(reversed));
+});
+
+server.post("/meme", (req, res) => {
+  let caption = JSON.parse(req.body);
+  let funny = [];
+
+  for (let i = 0; i < caption.length; i++) {
+    let letter = caption[i];
+    if (letter === " ") {
+      funny.push(letter);
+    } else {
+      if (i % 2 === 0) {
+        funny.push(letter.toUpperCase());
+      } else {
+        funny.push(letter.toLowerCase());
+      }
+    }
+  }
+  funny = funny.join("");
+  res.end(JSON.stringify(funny));
 });
