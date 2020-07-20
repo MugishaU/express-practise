5024636a3deb616312aaf8e32f62cd66a4526015
MugishaU <muwiragiye123@gmail.com>
2020-07-20 17:35:14 +0100

    edit meme display

--- client.js
+++ client.js
@@ -2,22 +2,22 @@ const r_form = document.getElementById("reverse_form");
 const result = document.getElementById("result");
 const m_form = document.getElementById("meme_form");
 
-r_form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let originalText = event.target.input.value;
+// r_form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   let originalText = event.target.input.value;
 
-  const options = {
-    method: "POST",
-    body: JSON.stringify(originalText),
-  };
+//   const options = {
+//     method: "POST",
+//     body: JSON.stringify(originalText),
+//   };
 
-  fetch("http://localhost:3000/reverse", options)
-    .then((r) => r.json())
-    .then((data) => {
-      result.textContent = data;
-    })
-    .catch(console.warn);
-});
+//   fetch("http://localhost:3000/reverse", options)
+//     .then((r) => r.json())
+//     .then((data) => {
+//       result.textContent = data;
+//     })
+//     .catch(console.warn);
+// });
 
 m_form.addEventListener("submit", (event) => {
   event.preventDefault();
